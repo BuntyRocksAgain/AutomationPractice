8d9c6c28856a6df9139b490006e3970ff20ee082
BuntyRocksAgain <157494867+BuntyRocksAgain@users.noreply.github.com>
2024-02-28 18:11:37 +0530

    Add files via upload

---
+++ CreateJSONPayload.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_Automation_Practice.API
+{
+    internal class CreateJSONPayload
+    {
+        public int Roll { get; set; }
+        public string Name { get; set; }
+        public List<string> Courses { get; set; }
+    }
+    public class Test
+    {
+        //public static void Main()
+        //{
+        //    try
+        //    {
+        //        CreateJSONPayload payload = new CreateJSONPayload()
+        //        {
+        //            Roll = 1,
+        //            Name = "Soumya",
+        //            Courses = new List<string>()
+        //        {
+        //            "Math", "Science", "English"
+        //        }
+        //        };
+        //        string stringjson = JsonConvert.SerializeObject(payload);
+        //        Console.WriteLine(stringjson);
+        //        Assert.AreEqual(payload.Courses[0], "Math");
+        //        Assert.AreEqual(payload.Name, "Soumya");
+        //        Assert.AreEqual(payload.Roll, 1);
+        //        Console.ReadLine();
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        Console.WriteLine("Problem in validating Payload");
+        //        Console.ReadLine();
+        //    }
+
+        //}
+    }
+}
---
+++ SeleniumTest.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_Automation_Practice
+{
+    internal class SeleniumTest
+    {
+        //public static void Main()
+        //{
+        //    try
+        //    {
+        //        IWebDriver driver = new ChromeDriver();
+        //        driver.Manage().Window.Maximize();
+        //        driver.Navigate().GoToUrl("https://www.flipkart.com/");
+        //        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        //        driver.FindElement(By.XPath("//span[@role='button']")).Click();
+        //        driver.FindElement(By.XPath("//a[@aria-label='Mobiles & Tablets']")).Click();
+        //        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        //        //Page Scroll down
+        //        IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+        //        executor.ExecuteScript("window.scrollBy(0, 1550)", "");
+
+               
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+        //}
+    }
+}
